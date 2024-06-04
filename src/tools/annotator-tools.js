0fb37a65e1da2917baccdbc5bfa5034cfc6be841
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2024-06-04 23:05:46 +0530

    toolbar is now appearing on the top of selected text

---
+++ src/tools/annotator-tools.js
@@ -0,0 +1,15 @@
+const textHighlighter = document.querySelector(".textHighlighter");
+const rectangle = document.querySelector(".rectangle");
+const circle = document.querySelector(".circle");
+const underline = document.querySelector(".underline");
+const pointer = document.querySelector(".pointer");
+const undo = document.querySelector(".undo");
+const redo = document.querySelector(".redo");
+
+console.log(textHighlighter)
+console.log(rectangle)
+console.log(circle)
+console.log(underline)
+console.log(pointer)
+console.log(undo)
+console.log(redo)
